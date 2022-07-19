47424f3d0e8aa6d01a53c200d27046e466c117fb
ALBERTO404 <albertgikandi01@gmail.com>
2022-07-19 13:24:12 +0300

    first commit

---
+++ add-students.php
@@ -0,0 +1,119 @@
+<?php
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "web2";
+
+// to connect the database and the form
+$conn = mysqli_connect($server,$username,$password,$database);
+
+if(isset($_POST["submitapplication"]))
+{
+    // 1. fetch form data
+    $fullname = $_POST["fullname"];
+    $phonenumber = $_POST["phonenumber"];
+    $email = $_POST["email"];
+    $gender = $_POST["gender"];
+    $course = $_POST["course"];
+
+    // 2. submit form data
+    $insertdata = mysqli_query($conn, "INSERT INTO enrollment(fullname,phonenumber,email,gender,course)VALUES('$fullname','$phonenumber','$email','$gender','$course')");
+    
+    if($insertdata)
+    {
+        echo "Data submitted successfully";
+    }
+    else
+    {
+        echo "Error".mysqli_error($conn);
+    }
+}
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<?php require_once('includes/headers.php')?>
+<body>
+	<!-- All our code. write here   -->
+	<?php require_once('includes/navbar.php')?>
+
+	<div class="sidebar">
+	<?php require_once('includes/sidebar.php')?>
+
+
+	</div>
+	<div class="main-content">
+		<div class="container-fluid">
+        <form action="enroll.php" method="POST" class="container">
+                    <h5 class="card-title">Sign up today?</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <label for="fullname" class="form-label">Full Name:</label>
+                            <input type="text" name="fullname" class="form-control" placeholder="Enter your full name">
+                        </div>
+                        <div class="mb-3 col-lg-6">
+                            <label for="phonenumber" class="form-label">Phone Number:</label>
+                            <input type="text" name="phonenumber" class="form-control" placeholder="+2547...">
+                        </div>
+                    </div>
+                    <div class="row" style="padding-top:3%;">
+                        <div class="col-lg-6">
+                            <label for="email" class="form-label">Email Address:</label>
+                            <input type="text" name="email" class="form-control" placeholder="Please enter your email address">
+                        </div>
+                        <div class="col-lg-6">
+                            <label for="gender" class="form-label"><b>What is your gender</b></label>
+                            <select name="gender" class="form-select" aria-label="Default select example">
+                                <option selected>--select your gender--</option>
+                                <option value="Male">Male</option>
+                                <option value="Female">Female</option>
+                            </select>
+                        </div>
+                    </div>   
+                    <div class="row" style="padding-top:5%;">
+                        <p>
+                            In order to complete your registration to the bootcamp, you are required to select one course you will be undertaking. Please NOTE that this will be your learning track during the 2-weeks immersion.
+                        </p>
+                    </div>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <label for="gender" class="form-label"><b>What's your preffered course</b></label>
+                        <select class="form-select" name="course"  aria-label="Default select example">
+                            <option selected>--select your course--</option>
+                            <option value="Software Development">Software Development</option>
+                            <option value="Data analysis">Data analysis</option>
+                        </select>
+                    </div>
+                </div>
+           
+                <div class="row text-cntre" style="padding-top:2%">
+                    <p">
+                        You agree by providing your information you understand all our data privacy and protection policy outlined in out Terms & condition and the Privacy Policy statements.
+                    </p>
+
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                        <label class="form-check-label" for="flexCheckDefault">Agree terms and condition.</label>
+                    </div>
+                    <div class="row" style="padding-top:8px;">
+                        <div class="col-lg-3">
+                            <button type="submit" name="submitapplication" class="btn btn-primary">submit application</button>
+                        </div>
+                    </div>
+                    
+                </div>
+                </form>
+
+			
+		</div>	
+	</div>
+	
+	<?php require_once('includes/scripts.php')?>
+</body>
+</html>
--- edit-contactus.php
+++ edit-contactus.php
@@ -1,111 +1,116 @@
-<?php
-$message ="";
-require_once('logics/dbconnection.php');
-$queryuser=mysqli_query($conn, "SELECT * FROM enrollment WHERE no='".$_GET['id']."'");
-
+<?php 
+$message = "";
+require_once("logics/dbconnection.php");
+$queryuser = mysqli_query($conn,"SELECT * FROM contactus WHERE no='".$_GET['id']."' ");
 
 while($fetchuser=mysqli_fetch_array($queryuser))
 {
-	$id= $fetchuser['no'];
-	$fullname=$fetchuser['fullname'];
-	$phonenumber= $fetchuser['phonenumber'];
-	$email= $fetchuser['email'];
-	$gender=$fetchuser['gender'];
-	$course=$fetchuser['course'];
+    $id = $fetchuser['no'];
+    $firstname = $fetchuser['firstname'];
+    $lastname = $fetchuser['lastname'];
+    $phonenumber = $fetchuser['phonenumber'];
+    $emailaddress = $fetchuser['email'];
+    $Message = $fetchuser['message'];
+    
 }
 
-//update user records
-if(isset($_POST['updateRecords']))
+// update user records
+if(isset($_POST["updateB"]))
 {
-	//fetch form data
-	$name=$_POST['fullname'];
-	$phone=$_POST['phonenumber'];
-	$emailaddress=$_POST['email'];
-	$genderform=$_POST['gender'];
-	$courseform=$_POST['course'];
+    // fetch form data
+    $fname = $_POST['firstname'];
+    $lname = $_POST['lastname'];
+    $number = $_POST['phonenumber'];
+    $emailA = $_POST['email'];
+    $MEssage = $_POST['message'];
 
-	//update records
-	$updateQuery=mysqli_query ($conn, "UPDATE enrollment SET fullname= '$fullname', phone='$phone', email='$email', gender='$genderform', course='$course' WHERE no='".$_GET['id']."'");
-	if($updateQuery)
-	{
-		$message="Data update";
-	}
-	else{
-		$message="error";
-	}
+
+    // update records
+    $updateQuery = mysqli_query($conn, "UPDATE contactus SET firstname='$fname', lastname='$lname', phonenumber='$number', email='$emailA' WHERE no='".$_GET['id']."'");
+
+    if($updateQuery)
+    {
+        $message= "Data Updated";
+    }
+    else
+    {
+        $message= "Error occured";
+    }
 }
-?>
 
+
+
+
+?>
 <!DOCTYPE html>
 <html>
-<?php require_once('includes/headers.php')?>
+<?php require_once("includes/headers.php")?>
 <body>
 	<!-- All our code. write here   -->
-	<?php require_once('includes/navbar.php')?>
+	
+	<?php require_once("includes/navbar.php")?>
 
 	<div class="sidebar">
-	<?php require_once('includes/sidebar.php')?>
-
-
+	<?php require_once("includes/sidebar.php")?>
 	</div>
+
 	<div class="main-content">
 		<div class="container-fluid">
-            <div class="row">
+			<div class="row">
                 <div class="col-lg-12">
                     <div class="card">
-                        <div class="card-header bg-dark text-center text-white">
-                            <h4>Edit Student:  </h4>
-							<span><?php echo $message ?></span>
+                        <div class="card-header bg-dark text-white text-center">
+                            <h4>Edit Student: <?php echo $firstname ?></h4>
+                            <span><?php echo $message ?></span>
                         </div>
-                		<div class="card-body">
-							<form action="edit-enrollment.php?id=<?php echo $id?>" method="POST">
-								<div class="row">
-									<div class="mb-3 col-lg-6 col-md-6 col-sm-12">
-										<label for="fullname" class="form-label">Full name</label>
-										<input type="text" class="form-control" name="fullname" value="<?php echo$fullname?>"placeholder="Enter your full name">
-									</div>
-									<div class="mb-3 col-lg-6 col-md-6 col-sm-12">
-										<label for="phonenumber" class="form-label">Phone </label>
-										<input type="tel" class="form-control" name="phonenumber" value="<?php echo$phonenumber?>" placeholder="+2547...">
-									</div>
-								</div>
-								<div class="row">
-									<div class="mb-3 col-lg-6 col-md-6 col-sm-12">
-										<label for="email" class="form-label">Email address</label>
-										<input type="email" class="form-control" name="email" value="<?php echo$email?>" placeholder="Please enter your email">
-									</div>
-									<div class="mb-3 col-lg-6 col-md-6 col-sm-12">
-										<label for="gender" class="form-label">What's your gender</label>
-										<select name="gender" class="form-control" aria-label="default select example">
-											<option value="<?php echo $gender?>">Male</option>
-											<option value="Female">Female</option>
-										</select>
-									</div>
-									
-									<div class="mb-3 col-lg-6">
-										<select name="course" class="form-control multiplchose_questiontypes" id="selector">
-											<option value="<?php echo $course?>">web design</option>
-											<option value="cyber security">cyber security</option>
-											<option value="android development">android development</option>
-
-										</select>
-									</div>    
-								</div>
-								<div class="row">
-									<div class="col-lg-6">
-										<button type="submit" class="btn btn-primary">Update records</button>
-									</div>
-								</div>
-        					</form>
-                    	</div>
-					</div>
-
+                        <div class="card-body">
+                            <form action="edit-contactus.php?id=<?php echo $id?>" method="POST" class="container">
+                                    <div class="row">
+                                        <div class="pb-3 col-lg-12">
+                                            <label for="Firstname" class="form-label">First name:</label>
+                                            <input type="text" name="firstname" value="<?php echo $firstname?>" class="form-control" placeholder="Enter your full name">
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="pb-3 col-lg-12">
+                                            <label for="lastname" class="form-label">Last Name:</label>
+                                            <input type="text" name="lastname" value="<?php echo $lastname?>" class="form-control" placeholder="+2547...">
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="pb-3 col-lg-12">
+                                            <label for="phonenumber" class="form-label">Phone Number:</label>
+                                            <input type="text" name="phonenumber" value="<?php echo $phonenumber?>" class="form-control" placeholder="Please enter your email address">
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="pb-3 col-lg-12">
+                                            <label for="email" class="form-label">Email Address:</label>
+                                            <input type="text" name="email" value="<?php echo $emailaddress?>" class="form-control" placeholder="Please enter your email address">
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="pb-3 col-lg-12">
+                                            <label for="message" class="form-label">Message:</label>
+                                            <input type="text" name="message" value="<?php echo $Message?>" class="form-control" placeholder="Please enter your email address">
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="mb-3 col-lg-12">
+                                            <button style="margin-top:10px" type="submit" name="updateB" class="btn btn-primary">Update</button>
+                                        </div>
+                                    </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
-			
-		</div>	
+		</div>
 	</div>
-	
-	<?php require_once('includes/scripts.php')?>
+
+
+
+	<?php require_once("includes/scripts.php")?>
+
 </body>
 </html>
